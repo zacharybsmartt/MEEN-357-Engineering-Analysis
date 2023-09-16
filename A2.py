5a1694c2e604d62c7fef41bc81300bb71ebff6f9
Zachary Bucknor-Smartt <zacharybsmartt@gmail.com>
2023-09-16 10:28:16 -0500

    yea

--- A2.py
+++ A2.py
@@ -13,7 +13,7 @@ def bisection(fun,lb,ub,err_max = 1e-8, iter_max = 1000):
     if type(ub) == int or type(ub) == float: pass
     else: raise Exception('The upper bound input type is incorrect. Input an integer value.')
     
-    if (type(err_max) == int or type(err_max) == float) and err_max >= 0: pass
+    if (type(err_max) == int or type(err_max) == float): pass
     else: raise Exception('The err_max input type is incorrect. Input an integer value.')
     
     if type(iter_max) == int or type(iter_max) == float: pass
@@ -45,74 +45,47 @@ def bisection(fun,lb,ub,err_max = 1e-8, iter_max = 1000):
 
     return root, err, numIter, exitFlag
 
-def secant(fun, ini_guess, err_max=1e-6, iter_max=10000):
+
+def secant(fun, ini_guess, err_max = 10e-6, iter_max = 10000):
+    global root, err, numIter, exitFlag
+    done = False
     xold = 100000000000000000000000000000
     xnew = ini_guess
     numIter = 0
+    step = 0.001
 
     if not callable(fun):
-        raise ValueError("The function 'fun' must be callable.")
-
-    if not isinstance(ini_guess, (int, float)):
-        raise ValueError("The initial guess must be a scalar value.")
-
-    if not (err_max > 0 and iter_max > 0):
-        raise ValueError("err_max and iter_max must be positive scalar values.")
-
-    while numIter < iter_max:
-        m = (fun(xnew) - fun(xnew + 1e-6)) / 1e-6  # Compute the slope numerically
-        if m == 0:
-            raise ValueError("Slope (m) is zero. Cannot proceed with the secant method.")
+        exitFlag = -2
+        return root, err, numIter, exitFlag
+    
+    if isinstance(ini_guess,(int,float)) == False:
+        exitFlag = -1
+        raise Exception('Exit Flag:', exitFlag, "The second inpt must be scaler values")
         
+    if ((err_max < 0) or (iter_max < 0) or (isinstance(err_max,(int,float))) or (isinstance(iter_max,(int,float))) ) == False:
+        exitFlag = -1
+        raise Exception('Exit Flag:', exitFlag, "The third and fourth inputs must be positive scaler values")
+    
+    while not done:
+        m = (fun(xnew)-fun((xnew + step)))/(-1 * step)
         b = fun(xnew) - m * xnew
-        xnew = -b / m
+        xnew = (-1 * b)/m
         numIter += 1
         err = abs((xnew - xold) / xnew) * 100
         xold = xnew
-
+        
         if err < err_max:
-            return xnew, err, numIter, 1  # Found a root
-
-    raise ValueError("Maximum number of iterations reached without finding a root.")
-# def secant(fun, ini_guess, err_max = 10e-6, iter_max = 10000):
-#     global root, err, numIter, exitFlag
-#     done = False
-#     xold = 100000000000000000000000000000
-#     xnew = ini_guess
-#     numIter = 0
-#     step = 0.001
-
-#     if not callable(fun):
-#         exitFlag = -2
-#         return root, err, numIter, exitFlag
-    
-#     if isinstance(ini_guess,(int,float)) == False:
-#         exitFlag = -1
-#         raise ValueError('Exit Flag:', exitFlag, "The second inpt must be scaler values")
+            exitFlag = 1
+            root = xnew
+            done = True
         
-#     if ((err_max < 0) or (iter_max < 0) or (isinstance(err_max,(int,float))) or (isinstance(iter_max,(int,float))) ) == False:
-#         exitFlag = -1
-#         raise ValueError('Exit Flag:', exitFlag, "The third and fourth inputs must be positive scaler values")
-    
-#     while not done:
-#         m = (fun(xnew)-fun((xnew + step)))/(-1 * step)
-#         b = fun(xnew) - m * xnew
-#         xnew = (-1 * b)/m
-#         numIter += 1
-#         err = abs((xnew - xold) / xnew) * 100
-#         xold = xnew
-        
-#         if err < err_max:
-#             exitFlag = 1
-#             root = xnew
-#             done = True
-        
-#         if numIter >= iter_max:
-#             exitFlag = 0
-#             root = xnew
-#             done = True
+        if numIter == iter_max:
+            exitFlag = 0
+            root = xnew
+            done = True
+            raise Exception('Exit Flag:', exitFlag, "Max iterations reached")
    
-#     return root, err, numIter, exitFlag
+    return root, err, numIter, exitFlag
 
 
 # Task 5
