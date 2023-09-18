3cbca9bd0c8d653c30455ec18b3bd5f14b2c3a1b
Zachary Bucknor-Smartt <zacharybsmartt@gmail.com>
2023-09-18 08:27:47 -0500

    subfunctions layout

---
+++ Project/Phase 1/subfunctions.py
@@ -0,0 +1,32 @@
+from math import *
+
+# save space above for imports if need be
+
+def get_mass(rover):
+    return m
+
+
+def get_gear_ratio(speed_reducer):
+    return Ng
+
+
+def tau_dcmotor(omega, motor):
+    return tau
+
+
+def F_drive(omega, rover):
+    return Fd
+
+
+def F_gravity(terrain_angle, rover, planet):
+    return Fgt
+
+
+def F_rolling(omega, terrain_angle, rover, planet, Crr):
+    return Frr
+
+
+def F_net(omega, terrain_angle, rover, planet, Crr):
+    return Fnet
+
+
