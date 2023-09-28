4d9f3fb793410335274f3fbb85e8ee6c46f5cccd
Mc-Mar <145362124+Mc-Mar@users.noreply.github.com>
2023-09-28 01:55:02 -0500

    Update analysis_terrain_slope.py

---
+++ Project/Phase 1/analysis_terrain_slope.py
@@ -0,0 +1,10 @@
+############ANALYSIS TERRAIN SLOPE##########
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.olptimize import root_scalar
+from subfunctions import*
+from define_rover import*
+
+#Assumed Coefficient Crr#
+Crr = 0.2
