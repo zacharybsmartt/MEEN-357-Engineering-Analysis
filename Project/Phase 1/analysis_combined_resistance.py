50748081dd55ac8862d929725e91917eddbe227c
Mc-Mar <145362124+Mc-Mar@users.noreply.github.com>
2023-09-29 13:34:12 -0500

    Update analysis_combined_resistance.py

--- Project/Phase 1/analysis_combined_resistance.py
+++ Project/Phase 1/analysis_combined_resistance.py
@@ -5,12 +5,13 @@ import numpy as np
 from subfunctions import*
 import matplotlib.pyplot as plt
 from define_rover import*
+#Need to account to different errors to graph using bisection
 from scipy.optimize import root_scalar
-#from basic_bisection_method import *
+
 
 #retrieve rover and planet values
 rover, planet = rover()
-gear_ratio = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
+gear_raio = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
 wheel_radius = rover['wheel_assembly']['wheel']['radius']
 #Create necessary Crr array
 Crr_array = np.linspace(0.01,0.4,25)
@@ -22,24 +23,33 @@ noload_speed = rover['wheel_assembly']['motor']['speed_noload']
 
 Crr, Slope = np.meshgrid(Crr_array,slope_array_deg)
 
+
 CRR, SLOPE = np.meshgrid (Crr_array, slope_array_deg)
+#initialize VMAX
 VMAX = np.zeros(np.shape(CRR), dtype = float)
 N = np.shape(CRR)[0]
 
+#LOOP#
 for i in range(N):
     for j in range(N):
         Crr_sample = float(CRR[i,j])
         slope_sample = float(SLOPE[i,j])
         #create function using F_net
         function = lambda omega: F_net(omega,slope_sample,rover,planet,Crr_sample)
-        solution = root_scalar(function,method = 'bisect', bracket = [0,noload_speed])
+        try:
+            solution = root_scalar(function,method = 'bisect', bracket = [0,noload_speed])
+        except ValueError:
+            VMAX[i,j] = np.nan
+        #if VMAX[i,j] != np.nan:
+            #VMAX[i,j] = (solution.root * wheel_radius) / gear_ratio
         VMAX[i,j] = (solution.root * wheel_radius) / gear_ratio
-
-figure = plt.figure()
-ax = Axes3D(figure, elev = 400, azim = 400)
-ax.plot_surface(CRR, SLOPE, VMAX)
-ax.set_title("Maximum Rover Speed vs. Crr vs. Terrain Angle")
-ax.set_ylabel("Terrain Angle [deg]")
-ax.set_zlabel("Maximum Rover Speed [m/s]")
-ax.set_xlable("Crr")
+        
+####GRAPHING####
+fig, ax = plt.subplots(subplot_kw={'projection' : '3d'})
+surf = ax.plot_surface(CRR, SLOPE, VMAX)
+ax.set_xlabel('Resistance Crr')
+ax.set_ylabel("Terrain ANgle [deg]")
+ax.set_title("Max Rover Speed vs. Terrain angles vs. Rolling Resistances")
+ax.set_zlabel('Max Rover Speed [m/s]')
 plt.show()
+
