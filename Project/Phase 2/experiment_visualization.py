3d25c904a05a83c39207b5182376b13ba8069923
Kontrabandito <Jvest200@gmail.com>
2023-10-26 02:43:36 -0500

    Update experiment_visualization.py

---
+++ Project/Phase 2/experiment_visualization.py
@@ -0,0 +1,16 @@
+from scipy.interpolate import interp1d
+import numpy
+import define_experiment as ex
+import matplotlib.pyplot as plotter
+
+alpha_dist = (experiment := ex.experiment1()[0])['alpha_dist']
+alpha_deg = experiment['alpha_deg']
+alphaInterpolationFunction = interp1d(alpha_dist, alpha_deg, kind = 'cubic', fill_value='extrapolate') #fit the cubic spline
+terrainSpace = numpy.linspace(min(alpha_dist), max(alpha_dist), 100)
+
+figure, subplot = plotter.subplots(1,1, figsize=(8,5))
+subplot.set_title('Terrain Angle at specific positions')
+subplot.set_xlabel('Position (m)')
+subplot.set_ylabel('Terrain Angle (deg)')
+subplot.plot(terrainSpace, alphaInterpolationFunction(terrainSpace))
+figure.show()
