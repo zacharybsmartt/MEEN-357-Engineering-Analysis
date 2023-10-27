f9ba930108504a58238f7a6fe1141eb0b5dedd4a
Mc-Mar <145362124+Mc-Mar@users.noreply.github.com>
2023-10-27 18:25:12 -0500

    Update rover_experiment1.py
    Fixed graph for rover_experiment1

--- Project/Phase 2/rover_experiment1.py
+++ Project/Phase 2/rover_experiment1.py
@@ -1,33 +1,39 @@
 import numpy as np
 from subfunctions import *
+from define_experiment import*
 import matplotlib.pyplot as plt
 
-# Load experiment data and simulate the rover's behavior
+#call the experiment1 function provided and assign the returns to experiment and end_event variables
 experiment, end_event = experiment1()
-end_events = end_event
-simulate_rover(rover, planet, experiment, end_events)
+#update the information for the end_event regarding the maximum time distance and minimum velocity
+end_event.update({'max_distance':1000,'max_time':10000,'min_velocity':0.01})
 
-# Define variables for graphing telemetry data
-times = rover['telemetry']['Time']
-positions = rover['telemetry']['position']
-velos = rover['telemetry']['velocity']
-pows = rover['telemetry']['power']
+#call simulate_rover
+simulate_rover(rover,planet,experiment,end_event)
+#assign the velocity values from the velocity located inside rover telemetry
+velocity = rover['telemetry']['velocity']
+#assign the power values to a variable from the position subsection loacated within the telemetry of the rover dictionary
+power = rover['telemetry']['power']
+#assign the position values to a variable from the postiion subsection located within telemetry of the rover dictionary
+position = rover['telemetry']['position']
+#assign the time values from the time subsection of telemetry within the rover dictionary
+time = rover['telemetry']['Time']
 
-# Create a 3x1 grid of subplots for the telemetry data
-fig, ax = plt.subplots(3, 1)
+
+#create the graphing layout
+fig, axs = plt.subplots(3, 1)
 fig.tight_layout(h_pad=4)
 
 # Plot the rover's position over time
-ax[0].plot(times, positions, 'bo', times, positions, 'b')  # Blue dots and lines
-ax[0].set(xlabel='Time (s)', ylabel='Position (m)', title='Position vs. Time')
-
+axs[0].plot(time,position,'bo', time, position, 'b')
+axs[0].set(xlabel='Time (s)', ylabel='Position (m)', title='Position vs. Time')
 # Plot the rover's velocity over time
-ax[1].plot(times, velos, 'ro', times, velos, 'r')  # Red dots and lines
-ax[1].set(xlabel='Time (s)', ylabel='Velocity (m/s)', title='Velocity vs. Time')
-
+axs[1].plot(time, velocity, 'ro', time, velocity, 'r')  # Red dots and lines
+axs[1].set(xlabel='Time (s)', ylabel='Velocity (m/s)', title='Velocity vs. Time')
 # Plot the rover's power consumption over time
-ax[2].plot(times, pows, 'go', times, pows, 'g')  # Green dots and lines
-ax[2].set(xlabel='Time (s)', ylabel='Power (W)', title='Power vs. Time')
+axs[2].plot(time, power, 'go', time, power, 'g')  # Green dots and lines
+axs[2].set(xlabel='Time (s)', ylabel='Power (W)', title='Power vs. Time')
+
 
 # Task 9 Calculations:
 # Calculate energy consumption and compare with the battery's energy
