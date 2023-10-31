7e306b958e5fae46a4de930b1aaa44302c1d605a
Zachary Bucknor-Smartt <zacharybsmartt@gmail.com>
2023-10-31 10:49:44 -0500

    lab start, to be continued next class

---
+++ Classwork-Labs/regression_lab.py
@@ -0,0 +1,11 @@
+import numpy as np
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+# least square fit linear regression for the function y = 30 + 2x + 3x^2 + E, E~(0,20)
+
+np.random.seed(6)
+x = np.linspace(0,9,10)
+x = x.reshape(len(x), 1)
+noise =  20 * np.random.normal(size=(np.size(x, 0), np.size(x, 1)))
+y = 30 + 2 * x + 3 * x ** 2 + noise
+
