3b88417063eb39e482b4c54526016c5d995ce56e
Zachary Bucknor-Smartt <zacharybsmartt@gmail.com>
2023-11-14 18:27:34 -0600

    hw6 setup

---
+++ Homework/A6.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.array([3.0, 4.5, 7.0, 9.0])
+fx = np.array([2.5, 1.0, 2.5, 0.5])
+xnew = np.linspace(3.0, 9.0, 100)
+xd = [0., 1., 2., 3., 4., 5., 6., 7., 8., 9.]
+yd = [38.824, 31.382, 106.615, 84.958, 136.192, 221.649, 239.815,
+326.167, 433.752, 527.402]
+
+
+def QuadraticSpline(x, fx):
+
+    return coeffs
+
+
+def QuadraticSplineInterp(x, xnew, coeffs):
+
+    return fapprox
+
+
+def BackSubstitutionSolver(U. b):
+    # is U a square matrix?
+    # is b a column vector?
+    # Do U and b have the same number of rows?
+    # Is U an upper triangular matrix?
+    
+    return sol
+
+
+def LinRegress(xd, yd, n):
+
